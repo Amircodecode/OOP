1297e30ef78e03140b15aaf295c1cea5bb5452b7
Amirjon Nazarov <a3465172@gmail.com>
2025-01-13 08:14:53 +0500

    D

--- classes.cpp
+++ classes.cpp
@@ -626,12 +626,30 @@ class Point {
 
         bool operator == (const Point & other) {
             return this->x == other.x && this->y == other.y;
+        } 
 
-        } bool operator != (const Point & other) {
+        bool operator != (const Point & other) {
             return !(this->x == other.x && this->y == other.y);
         }
 
+        bool operator > (const Point & other) {
+            return this->x > other.x && this->y > other.y;
+        }
 
+        bool operator < (const Point & other) {
+            return this->x < other.x && this->y < other.y;
+        }
+
+
+        bool operator >= (const Point & other) {
+            return this->x >= other.x && this->y >= other.y;
+        }
+
+        bool operator <= (const Point & other) {
+            return this->x <= other.x && this->y <= other.y;
+        }
+
+        
         int getX() {
             return x;
         }
@@ -664,7 +682,7 @@ int main() {
    
     Point a(5, 6);
     Point b(6, 8);
-    bool result = a!=b;
+    bool result = a > b;
 
     cout << result << endl;
 
