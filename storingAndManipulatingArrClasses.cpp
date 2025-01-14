ff85e67d6a94bf030565a0552338412a5d62e3e4
Amirjon Nazarov <a3465172@gmail.com>
2025-01-14 07:29:57 +0500

    D

--- storingAndManipulatingArrClasses.cpp
+++ storingAndManipulatingArrClasses.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Myclass {
     public:
         Myclass(int size) {
+            this->Size = size;
             this->arr = new int[size];
             cout << "enter " << size << " elements " << endl;
             for (int i = 0; i < size; i++) {
@@ -11,19 +12,46 @@ class Myclass {
                 cin >> arr[i];
             }
             for(int i = 0; i < size; i++) {
-                cout << arr[i];
+                cout << arr[i] << endl;
             }
+        }
+
+        void PrintArr() {
+            cout << "the array consist of " << Size << " elements " << endl;
+            for (int i = 0; i < Size; i++) {
+                cout << arr[i] << endl;
+            }
+        }
+
+        ~Myclass() {
+            cout << "the destructor was called " << this << endl;
+            delete[] arr;
+            cout << "the array was cleared" << endl;
         }
     private:
         int *arr;
+        int Size;
 
 
 };
 
 
 int main() {
+    int size;
+    cout << "enter the size: ";
+    cin >> size;
+    Myclass arr1(size);
+    int operation;
+    do {
+        cout << "you can " << endl <<
+                "1. modify the arr" << endl <<
+                "2. modify the arr" << endl << 
+                "if you want to exit enter 0" << endl;
+        cout << ">";
+        cin >> operation;
+            
 
-    Myclass arr1(2);
+    } while(operation == 0);
 
     return 0;
 }
