ced60ad83a25de146296d16cc07f5b47caa011f5
Amirjon Nazarov <a3465172@gmail.com>
2025-01-14 07:13:06 +0500

    D

--- storingAndManipulatingArrClasses.cpp
+++ storingAndManipulatingArrClasses.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 using namespace std;
 
+class Myclass {
+    public:
+        Myclass(int size) {
+            this->arr = new int[size];
+            cout << "enter " << size << " elements " << endl;
+            for (int i = 0; i < size; i++) {
+                cout << "enter " << i + 1 << " number: ";
+                cin >> arr[i];
+            }
+            for(int i = 0; i < size; i++) {
+                cout << arr[i];
+            }
+        }
+    private:
+        int *arr;
 
+
+};
 
 
 int main() {
 
-
+    Myclass arr1(2);
 
     return 0;
 }
